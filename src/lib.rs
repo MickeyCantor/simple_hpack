ae54deb30cb97917bf7017f584c2912b0c98e7f8
Brad Nowacki <BradNowacki@gmail.com>
2019-04-14 15:24:20 -0400

    wip commit before header refactor into tuple

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,64 @@
 use std::collections::HashSet;
+use lazy_static::lazy_static;
+use std::str;
 
 pub struct Hpack{
-    static_table: HashSet<Header>,
-    dynamic_table: HashSet<Header>,
-    dynamic_table_size: usize,
+    dynamic_table: DynamicTable,
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+pub struct DynamicTable{
+    table: Vec<Header>,
+    table_size: usize,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
 pub struct Header {
     name: String,
-    value: String,
+    value: Option<String>,
     index: usize,
-    indexed: Indexed,
 }
 
-impl Header{
-    pub fn new(name: String, value: String, index: usize, indexed: Indexed) -> Header {
-        Header{name: name, value: value, index: index, indexed: indexed}
+impl PartialEq<u32> for Header{
+    fn eq(&self, other: &u32) -> bool {
+        &(self.index as u32) == other
     }
 }
 
-#[derive(Hash, Eq, PartialEq, Debug)]
-pub enum Indexed {
-    Yes,
-    No,
-    Never,
+impl Header{
+    pub fn new(name: String, value: Option<String>, index: usize) -> Header {
+        Header{name: name, value: value, index: index}
+    }
+}
+
+impl DynamicTable {
+    pub fn new(dynamic_table_size: usize) -> DynamicTable {
+        DynamicTable{table: Vec::new(), table_size: dynamic_table_size}
+    }
+
+    /// Function used to add an entry to the dynamic table in FIFO format as per [IETF RFC 7541 Section 2.3](https://tools.ietf.org/html/rfc7541#section-2.3.2)
+    /// 
+     /// ## Arguments
+    /// 
+    /// * header - the Header you wish to insert into the dyamic table 
+    /// 
+    /// ## Returns
+    /// 
+    /// Nothing
+    pub fn add(&mut self, mut header: Header) {
+        for index in 0..self.table.len(){
+            let mut header = self.table.remove(0);
+            header.index = header.index + 1;
+            self.table.insert(index, header);
+        }
+
+        header.index = 62;
+        self.table.push(header);
+    }
 }
 
 impl Hpack{
-    pub fn new(dynamic_table_size: usize,static_table: Option<HashSet<Header>>) -> Hpack{
-        Hpack{static_table: HashSet::new(), dynamic_table: HashSet::new(), dynamic_table_size: dynamic_table_size}
+    pub fn new(dynamic_table_size: usize) -> Hpack{
+        Hpack{dynamic_table: DynamicTable::new(dynamic_table_size)}
     }
 
     ///Function used to read in a stream of headers, and convert them into a list of headers for consumption. 
@@ -42,8 +71,62 @@ impl Hpack{
     /// 
     /// 
     /// 
-    pub fn read_headers(&self, stream: Vec<u8>) -> Result<Vec<Header>,&'static str>{
-        Err("Write me!")
+    pub fn read_headers(&mut self, stream: Vec<u8>) -> Result<Vec<Header>,&'static str>{
+        match stream.get(0) {  
+            Some(x) => {
+                if (x >> 7) == 1_u8 {
+                    self.process_indexed(stream)
+                }else if (x >> 6) == 1_u8{
+                    self.process_indexed_literal(stream)
+                }else{
+                    Err("Write me! 3")
+                }
+            },
+            None => Ok(Vec::new()),
+        }
+    }
+
+    fn process_indexed(&mut self, stream: Vec<u8>) -> Result<Vec<Header>, &'static str> {
+        let (int, stream) = decode_int(stream, 7);
+        let mut vec = self.read_headers(stream)?;
+        vec.insert(0, self.get_static_entry_from_index(int)?.clone());
+        Ok(vec)
+    }
+
+    fn get_static_entry_from_index(&self, i: u32) -> Result<Header, &'static str> {
+        match STATIC_TABLE.iter().find(|&x|{x==&i}) {
+            Some(x) => Ok(x.clone()),
+            None => Err("Write Me! 2"),
+        }
+    }
+
+    fn process_indexed_literal(&mut self, stream: Vec<u8>) -> Result<Vec<Header>, &'static str> {
+        let (index, stream) = decode_int(stream, 6);
+            let (length, mut stream) = decode_int(stream, 7);
+            let range = length as usize;
+
+            println!{"index - {}, length - {}, range - {}",index,length,range};
+
+
+
+            match str::from_utf8(&stream.as_slice()[..range]) {
+                Ok(x) => {
+                    let value = String::from(x);
+                    for _ in 0..length {
+                        stream.remove(0);
+                    }
+
+                    let mut vec = self.read_headers(stream)?;
+                    let mut header = self.get_static_entry_from_index(index)?.clone();
+                    
+                    header.value = Some(value);
+                    self.dynamic_table.add(header.clone());
+                    vec.insert(0, header);
+                    
+                    Ok(vec)
+                },
+                Err(_) => Err("Write me! 1")
+        }
     }
 }
 
@@ -77,7 +160,7 @@ pub fn new_indexed(number: u32) -> Result<Vec<u8>,&'static str>{
 /// ## Returns
 /// 
 ///  * Result<Vec<u8>,&'static str> - a result containing the Vector of bytes or an error string
-pub fn new_literal(value: &str, index: u32, name: Option<&str>, huffman: bool) -> Result<Vec<u8>, &'static str>{
+pub fn new_literal(value: &str, index: u32, name: Option<&str>, _huffman: bool) -> Result<Vec<u8>, &'static str>{
     let build_literal = |index, value: &str| {
             if index == 0 {
                 Err(ERROR_INDEX_ZERO)
@@ -206,6 +289,74 @@ fn mask_first_byte(vec: Vec<u8>, mask: u8) -> Vec<u8> {
 }
 
 static ERROR_INDEX_ZERO: &str = "Error - Indexed field cannot be zero";
+lazy_static! {
+    ///Static header list as defined by [IETF RFC 7541 Section 5.1](https://tools.ietf.org/html/rfc7541#appendix-A)
+    static ref STATIC_TABLE: HashSet<Header> = {
+        let mut table = HashSet::new();
+        table.insert(Header::new(String::from(":authority"), None, 1));
+        table.insert(Header::new(String::from(":method"), Some(String::from("GET")), 2));
+        table.insert(Header::new(String::from(":method"), Some(String::from("POST")), 3));
+        table.insert(Header::new(String::from(":path"), Some(String::from("/")), 4));
+        table.insert(Header::new(String::from(":path"), Some(String::from("/index.html")), 5));
+        table.insert(Header::new(String::from(":scheme"), Some(String::from("http")), 6));
+        table.insert(Header::new(String::from(":scheme"), Some(String::from("https")), 7));
+        table.insert(Header::new(String::from(":status"), Some(String::from("200")), 8));
+        table.insert(Header::new(String::from(":status"), Some(String::from("204")), 9));
+        table.insert(Header::new(String::from(":status"), Some(String::from("206")), 10));
+        table.insert(Header::new(String::from(":status"), Some(String::from("304")), 11));
+        table.insert(Header::new(String::from(":status"), Some(String::from("400")), 12));
+        table.insert(Header::new(String::from(":status"), Some(String::from("404")), 13));
+        table.insert(Header::new(String::from(":status"), Some(String::from("500")), 14));
+        table.insert(Header::new(String::from("accept-charset"), None, 15));
+        table.insert(Header::new(String::from("accept-encoding"), Some(String::from("gzip, deflate")), 16));
+        table.insert(Header::new(String::from("accept-language"), None, 17));
+        table.insert(Header::new(String::from("accept-ranges"), None, 18));
+        table.insert(Header::new(String::from("accept"), None, 19));
+        table.insert(Header::new(String::from("access-control-allow-origin"), None, 20));
+        table.insert(Header::new(String::from("age"), None, 21));
+        table.insert(Header::new(String::from("allow"), None, 22));
+        table.insert(Header::new(String::from("authorization"), None, 23));
+        table.insert(Header::new(String::from("cache-control"), None, 24));
+        table.insert(Header::new(String::from("content-disposition"), None, 25));
+        table.insert(Header::new(String::from("content-encoding"), None, 26));
+        table.insert(Header::new(String::from("content-language"), None, 27));
+        table.insert(Header::new(String::from("content-length"), None, 28));
+        table.insert(Header::new(String::from("content-location"), None, 29));
+        table.insert(Header::new(String::from("contant-range"), None, 30));
+        table.insert(Header::new(String::from("content-type"), None, 31));
+        table.insert(Header::new(String::from("cookie"), None, 32));
+        table.insert(Header::new(String::from("date"), None, 33));
+        table.insert(Header::new(String::from("etag"), None, 34));
+        table.insert(Header::new(String::from("expect"), None, 35));
+        table.insert(Header::new(String::from("expires"), None, 36));
+        table.insert(Header::new(String::from("from"), None, 37));
+        table.insert(Header::new(String::from("host"), None, 38));
+        table.insert(Header::new(String::from("if-match"), None, 39));
+        table.insert(Header::new(String::from("if-modified-since"), None, 40));
+        table.insert(Header::new(String::from("if-none-match"), None, 41));
+        table.insert(Header::new(String::from("if-range"), None, 42));
+        table.insert(Header::new(String::from("if-unmodified-since"), None, 43));
+        table.insert(Header::new(String::from("last-modified"), None, 44));
+        table.insert(Header::new(String::from("link"), None, 45));
+        table.insert(Header::new(String::from("location"), None, 46));
+        table.insert(Header::new(String::from("max-forwards"), None, 47));
+        table.insert(Header::new(String::from("proxy-authenticate"), None, 48));
+        table.insert(Header::new(String::from("proxy-authorization"), None, 49));
+        table.insert(Header::new(String::from("range"), None, 50));
+        table.insert(Header::new(String::from("referer"), None, 51));
+        table.insert(Header::new(String::from("refresh"), None, 52));
+        table.insert(Header::new(String::from("retry-after"), None, 53));
+        table.insert(Header::new(String::from("server"), None, 54));
+        table.insert(Header::new(String::from("set-cookie"), None, 55));
+        table.insert(Header::new(String::from("strict-transport-security"), None, 56));
+        table.insert(Header::new(String::from("transfer-encoding"), None, 57));
+        table.insert(Header::new(String::from("user-agent"), None, 58));
+        table.insert(Header::new(String::from("vary"), None, 59));
+        table.insert(Header::new(String::from("via"), None, 60));
+        table.insert(Header::new(String::from("www-authenticate"), None, 61));
+        table
+    };
+}
 
 #[cfg(test)]
 mod tests {
@@ -305,14 +456,45 @@ mod tests {
 
     #[test]
     fn test_read_headers_static_indexed(){
-        let hpack = Hpack::new(128, None);
+        let mut hpack = Hpack::new(128);
 
         let stream = vec![130_u8,132_u8];
 
-        let expected = vec![Header::new(String::from(":method"), String::from("GET"), 2, Indexed::Yes)];
+        let expected = vec![Header::new(String::from(":method"), Some(String::from("GET")), 2),
+                            Header::new(String::from(":path"), Some(String::from("/")), 4)];
 
         assert_eq!(expected,hpack.read_headers(stream).unwrap())
     }
+
+    #[test]
+    fn test_read_headers_literal_indexed(){
+        let mut hpack = Hpack::new(128);
+
+        let stream = vec![66_u8, 3_u8, 0x47, 0x45, 0x54, 79_u8, 3_u8, 0x73, 0x65, 0x74];
+
+        let header_1 = Header::new(String::from(":method"), Some(String::from("GET")), 2);
+        let header_2 = Header::new(String::from("accept-charset"), Some(String::from("set")), 15);
+
+        let expected = vec![header_1.clone(), header_2.clone()];
+
+        assert_eq!(expected, hpack.read_headers(stream).unwrap());
+    }
+
+    #[test]
+    fn test_read_headers_dynamic_indexed(){
+        let mut hpack = Hpack::new(128);
+
+        let stream = vec![66_u8, 3_u8, 0x47, 0x45, 0x54, 79_u8, 3_u8, 0x73, 0x65, 0x74];
+
+        let header_1 = Header::new(String::from(":method"), Some(String::from("GET")), 2);
+        let header_2 = Header::new(String::from("accept-charset"), Some(String::from("set")), 15);
+        let header_3 = Header::new(String::from("accept-charset"), Some(String::from("set")), 63);
+
+        let expected = vec![header_1.clone(), header_2.clone()];
+
+        assert_eq!(expected, hpack.read_headers(stream).unwrap());
+    }
+
 
 }
 
