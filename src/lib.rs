eb11a99ef58a1fb558a754efa30fa92a5afd7879
Brad Nowacki <BradNowacki@gmail.com>
2019-04-23 20:25:51 -0400

    Added size setting for dynamic table

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use lazy_static::lazy_static;
 use std::str;
 
@@ -6,9 +5,11 @@ pub struct Hpack{
     dynamic_table: DynamicTable,
 }
 
+
 pub struct DynamicTable{
     table: Vec<(String,String)>,
     table_size: usize,
+    current_size: usize,
 }
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
@@ -18,21 +19,78 @@ pub struct Header {
 }
 
 impl DynamicTable {
+    /// Builds a new dynamic table of a given size in bytes, fucntions as a FIFO list of headers as per [IETF RFC 7541 Section 4](https://tools.ietf.org/html/rfc7541#section-4)
+    /// 
+    /// ## Arguments
+    /// 
+    /// * dynamic_table_size - the size in bytes of the table
+    /// 
+    /// ## Returns
+    /// 
+    /// A new dynamic table with no values.
     pub fn new(dynamic_table_size: usize) -> DynamicTable {
-        DynamicTable{table: Vec::new(), table_size: dynamic_table_size}
+        DynamicTable{table: Vec::new(), table_size: dynamic_table_size, current_size: 0}
     }
 
     /// Function used to add an entry to the dynamic table in FIFO format as per [IETF RFC 7541 Section 2.3](https://tools.ietf.org/html/rfc7541#section-2.3.2)
     /// 
-     /// ## Arguments
+    /// ## Arguments
     /// 
     /// * header - the Header you wish to insert into the dyamic table 
     /// 
     /// ## Returns
     /// 
+    /// An error if the header is larger then the table size
+    pub fn add(&mut self, header: (String,String)) -> Result<(),&'static str>{
+        let header_size = header.0.capacity() + header.1.capacity() + 32;
+        if header_size > self.table_size {
+            Err("Header exceeds table size!")
+        } else {
+            println!("Adding header - {:?}, size - {}",header, header_size);
+            let reamining_space = self.table_size - self.current_size;
+
+            if reamining_space < header_size{
+                println!("Removing header! header_size - {}, remaining_size - {}", header_size, reamining_space);
+                self.reduce_size(self.table_size - header_size);
+            }
+
+            self.current_size = self.current_size + header_size;
+            self.table.insert(0, header);
+            Ok(())
+        }
+       
+    }
+
+    /// Function used to set the table size, removing any elements that need to be removed
+    pub fn set_size(&mut self, new_size: usize){
+        
+        if new_size >= self.table_size {
+            self.table_size = new_size;
+        } else {
+            self.table_size = new_size;
+            self.reduce_size(new_size);
+        }
+    }
+
+    /// Function used to reduce the size of the table to lessthan or equal to the given size, removing any elements from the end of the vector as needed 
+    /// 
+    /// ## Arguments
+    /// 
+    /// * new_size - the new size you wish to set the table to
+    /// 
+    /// ## Returns 
+    /// 
     /// Nothing
-    pub fn add(&mut self, header: (String,String)) {
-        self.table.insert(0, header);
+    fn reduce_size(&mut self, new_size: usize){
+        println!("cur size - {}, new size - {}", self.current_size, new_size);
+        while self.current_size > new_size {
+            let header = self.table.pop();
+            println!("Removing - {:?}, cur size - {}", header, self.current_size);
+            match header {
+                Some(x) => self.current_size = self.current_size - (x.0.capacity() + x.1.capacity() + 32),
+                None => panic!("Oh boy batman, i shouldent be here!")
+            } 
+        }
     }
 }
 
@@ -58,12 +116,16 @@ impl Hpack{
                     self.process_indexed(stream)
                 }else if (x >> 6) == 1_u8{
                     self.process_indexed_literal(stream)
+                }else if (x >> 5) == 1_u8{
+                    let (size, stream) = decode_int(stream, 5);
+                    self.dynamic_table.table_size = size as usize;
+                    self.read_headers(stream)
                 }else if (x >> 4) == 0_u8 {
                     self.process_non_indexed_literal(stream)
                 }else if (x >> 4) == 1_u8 {
                     self.process_never_indexed_literal(stream)
-                } else {
-                    Err("Write me 7!")
+                }else {
+                    Err("Invalid start of header")
                 }
             },
             None => Ok(Vec::new()),
@@ -115,8 +177,6 @@ impl Hpack{
     fn get_string(stream: Vec<u8>) -> (Vec<u8>, String){
         let (length, mut stream) = decode_int(stream, 7);
             let range = length as usize;
-
-            println!("length - {}, remining vector - {:?}", length, stream);
 
             let value = match str::from_utf8(&stream.as_slice()[..range]) {
                 Ok(x) => String::from(x),
@@ -653,12 +713,102 @@ mod tests {
     fn test_read_headers_literial_never_indexed_dosent_get_indexed_with_name(){
         let mut hpack = Hpack::new(128);
         let stream = vec![16_u8, 7_u8, 0x3a, 0x6d, 0x65, 0x74, 0x68, 0x6f, 0x64, 3_u8, 0x47, 0x45, 0x54];
-        hpack.read_headers(stream);
+        hpack.read_headers(stream).unwrap();
 
         let stream = vec![192_u8];
 
         assert_eq!("Error index outside of dynamic table space", hpack.read_headers(stream).unwrap_err());
     }
 
+    #[test]
+    fn test_change_table_size(){
+        let mut hpack = Hpack::new(128);
+        let stream = vec![63_u8, 154_u8, 10_u8, 2_u8, 3_u8, 0x47, 0x45, 0x54];
+        let header_1 = Header{value: (String::from(":method"),String::from("GET")), indexed: true};
+        let expected = vec![header_1.clone()];
+
+        assert_eq!(expected,hpack.read_headers(stream).unwrap());
+        assert_eq!(1337, hpack.dynamic_table.table_size);
+    }
+
+    #[test]
+    fn test_dynamic_table_add(){
+        let mut table = DynamicTable::new(50);
+
+        table.add((String::from("This"),String::from("Fits"))).unwrap();
+
+        assert!(table.table.contains(&(String::from("This"), String::from("Fits"))))
+    }
+
+    #[test]
+    fn test_dynamic_table_add_too_large(){
+        let mut table = DynamicTable::new(10);
+
+        assert!(table.add((String::from("This is too large!"), String::from("Still too long"))).is_err())
+    }
+
+    #[test]
+    fn test_dynamic_table_add_removes_oldest(){
+        let mut table = DynamicTable::new(83);
+
+        table.add((String::from("Test"), String::from("Head"))).unwrap();
+        table.add((String::from("Test"), String::from("Head2"))).unwrap();
+        table.add((String::from("Test"), String::from("Head3"))).unwrap();
+
+        assert!(!table.table.contains(&(String::from("Test"), String::from("Head"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head2"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head3"))));
+    }
+
+    #[test]
+    fn test_dynamic_table_add_exact_size(){
+        let mut table = DynamicTable::new(81);
+
+        table.add((String::from("Test"), String::from("Head"))).unwrap();
+        table.add((String::from("Test"), String::from("Head2"))).unwrap();
+
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head2"))));
+    }
+
+    #[test]
+    fn test_dynamic_table_add_removes_oldest_to_exact_size(){
+        let mut table = DynamicTable::new(82);
+
+        table.add((String::from("Test"), String::from("Head"))).unwrap();
+        table.add((String::from("Test"), String::from("Head2"))).unwrap();
+        table.add((String::from("Test"), String::from("Head3"))).unwrap();
+
+        assert!(!table.table.contains(&(String::from("Test"), String::from("Head"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head2"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head3"))));
+    }
+
+    #[test]
+    fn test_dynamic_table_set_size_removes_oldest(){
+        let mut table = DynamicTable::new(83);
+
+        table.add((String::from("Test"), String::from("Head"))).unwrap();
+        table.add((String::from("Test"), String::from("Head2"))).unwrap();        
+
+        table.set_size(68);
+
+        assert!(!table.table.contains(&(String::from("Test"), String::from("Head"))));
+        assert!(table.table.contains(&(String::from("Test"), String::from("Head2"))));
+    }
+
+    #[test]
+    fn test_dynamic_table_set_size_zero(){
+        let mut table = DynamicTable::new(83);
+
+        table.add((String::from("Test"), String::from("Head"))).unwrap();
+        table.add((String::from("Test"), String::from("Head2"))).unwrap();
+
+        table.set_size(0);
+
+        assert!(table.table.is_empty());
+    }
+
+    
 }
 
